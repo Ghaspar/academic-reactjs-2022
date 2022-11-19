beea20c70cac9c769d59b10ba9cdb231a258387e
Lucas <lucas.ghaspar@gmail.com>
2022-11-18 22:31:27 -0300

    chore: header of app

--- feed/src/components/Header.tsx
+++ feed/src/components/Header.tsx
@@ -1,13 +1,15 @@
 import style from './styles.module.scss'
+import igniteLogo from '../assets/images/Ignite_simbol.svg'
 
 export function Header() {
 
     return(
         <>
-            <header className={style.header + " bg-secondary"}>
+            <header className={style.header + " bg-secondary py-3"}>
                 <div className="container">
-                    <div className="row">
-                        <h1>header teste</h1>
+                    <div className="d-flex align-items-center justify-content-center">
+                        <img src={igniteLogo} className={style.logo + " me-2"} alt="logo" />
+                        <h1>Ignite Feed</h1>
                     </div>
                 </div>
             </header>
