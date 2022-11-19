25af66814af32c01f1bcc3d3d710737528409f82
Lucas <lucas.ghaspar@gmail.com>
2022-11-19 11:22:25 -0300

    chore: make aside profile base

--- feed/src/components/sidebar/index.tsx
+++ feed/src/components/sidebar/index.tsx
@@ -4,11 +4,25 @@ export function Sidebar(){
 
     return(
         <>
-            <nav className="sidebar d-flex">
-                <ul>
-                    <li>teste</li>
-                </ul>
-            </nav>
+            <aside className={style.sidebar}>
+                <img 
+                    src="https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=80" 
+                    className={style.cover}
+                    alt="background perfil" 
+                />
+                <article>
+                    <div className={style.profile + " d-flex flex-column text-center"}>
+                        <strong className="text-white">Lucas</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </article>
+                <footer>
+                    <a href="#">
+                        <i className="bi bi-pencil-square"></i>
+                        Editar Perfil
+                    </a>
+                </footer>
+            </aside>
         </>
     );
 }
