f71f14d3f4ccfd72b7571998418c334be21728d8
Lucas <lucas.ghaspar@gmail.com>
2022-11-19 10:36:23 -0300

    chore: impor sidebar

--- feed/src/App.tsx
+++ feed/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Post } from '../src/Post'
 import { Header } from './components/Header'
+import { Sidebar } from './components/sidebar'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +12,7 @@ function App() {
       <div className="container">
         <div className="row">
           <div className="col-md-4">
-
+            <Sidebar />
           </div>
           <div className="col-md-8">
             <Post author="Lucas" coments="lorem testestsedf w sdf"/>
