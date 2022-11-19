971b5b73611f9f2729cf5f6ea747434f9baaaa78
Lucas <lucas.ghaspar@gmail.com>
2022-11-18 21:12:48 -0300

    chore: initialize structure with component

--- feed/src/App.tsx
+++ feed/src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import { Post } from '../src/Post'
+import { Header } from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
       <h1>hello word</h1>
       <Post author="Lucas" coments="lorem testestsedf w sdf"/>
     </>
---
+++ feed/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import style from './styles.module.css'
+
+export function Header() {
+
+    return(
+        <>
+            <header className={style.header}>
+                header teste
+            </header>
+        </>
+    );
+}
